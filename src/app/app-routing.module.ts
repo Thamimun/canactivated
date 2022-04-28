afa9b1936b3a8333df72e081115c634f2e9fdcb1
Thamimun <thamimum.a@simbiotikgroup.com>
2022-04-28 12:42:28 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardservice } from './auth-guard/auth-guard.component';
+import { dashboardComponent } from './dashboard.component';
+import { loginComponent } from './login.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[{path:'' ,component:loginComponent},
+  {path:'login' ,component:loginComponent},
+{path:'dashboard' ,component:dashboardComponent,canActivate:[AuthGuardservice]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
